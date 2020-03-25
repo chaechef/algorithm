f022825a7291887e1f087d29876d680d62e81e9c
chaechef <chaeefe@gmail.com>
2020-03-25 16:28:09 +0900

    array.go

--- basic/array.go
+++ basic/array.go
@@ -5,23 +5,25 @@ import (
 )
 
 func main() {
+	//array definition
 	arr := []int{1, 2, 3, 4, 5}
 	PrintArray(arr)
 
+	//array by make
 	arr1 := make([]int, 5)
 	for i := 0; i < 5; i++ {
 		arr1[i] = i * 10
 	}
 	PrintArray(arr1)
 
+	//array push
 	for i := 0; i < 5; i++ {
 		arr1 = append(arr1, i+1)
 	}
-	//push element
 	PrintArray(arr1)
 
+	//array slice
 	arr2 := arr1[4:10]
-	//slice array
 	PrintArray(arr2)
 
 }
