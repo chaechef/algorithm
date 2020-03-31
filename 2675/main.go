8d9542497a2a2962e4401e94493cbadd59fff47b
chaechef <chaeefe@gmail.com>
2020-03-31 19:32:07 +0900

    문자열붙이기

--- 2675/main.go
+++ 2675/main.go
@@ -1,1 +1,32 @@
-package _675
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var tc int
+	var m int
+	var str string
+
+	fmt.Fscanln(r, &tc)
+
+	for i := 0; i < tc; i++ {
+		var res string
+		fmt.Fscanf(r, "%d %s\n", &m, &str)
+		for j := 0; j < len(str); j++ {
+			for k := 0; k < m; k++ {
+				res += string(str[j])
+			}
+		}
+		fmt.Println(res)
+
+	}
+
+}
--- 2675/sub.go
+++ 2675/sub.go
@@ -1,1 +1,29 @@
-package _675
+package _2675
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+func recursion (mul int, str string) string {
+	res := ""
+	for i := 0; i < len(str); i++ {
+		res += strings.Repeat(string(str[i]), mul)
+	}
+	return res
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var tc int
+	fmt.Fscanln(r, &tc)
+	for i := 0; i < tc; i++ {
+		var mul int
+		var str string
+		fmt.Fscanln(r, &mul, &str)
+		res := recursion(mul, str)
+		fmt.Println(res)
+	}
+}
+
