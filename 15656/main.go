08596a862e9f967b86112c2d77245ec168f85f40
chaechef <chaeefe@gmail.com>
2020-04-03 16:05:24 +0900

    n 과m 2

---
+++ 15656/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func backtracking(checks []bool ,picked []int, num int, topick int)  {
+	l := len(picked)
+
+	if l > 1 && picked[l-2] > picked[l-1]   {
+		return
+	}
+
+	if topick == 0 {
+		//print
+		for i := 0; i < len(picked); i++ {
+			fmt.Fprint(w, picked[i], " ")
+		}
+		fmt.Fprint(w, "\n")
+		return
+	}
+
+	for i := 1; i <= num; i++ {
+		if checks[i] == false {
+			picked = append(picked, i)
+			checks[i] = true
+			backtracking(checks ,picked, num, topick-1)
+			checks[i] = false
+			picked = picked[:len(picked)-1]
+		}
+	}
+}
+func main() {
+
+	defer w.Flush()
+	var n, m int
+	var arr []int
+	fmt.Fscanln(r, &n, &m)
+	checks := make([]bool, n+1)
+
+	backtracking( checks, arr, n, m )
+}
