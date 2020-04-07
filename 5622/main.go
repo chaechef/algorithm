8edfe13bc8b89348e0409a8f09722119a00075e5
chaechef <chaeefe@gmail.com>
2020-04-07 17:22:25 +0900

    다이얼

--- 5622/main.go
+++ 5622/main.go
@@ -1,1 +1,28 @@
-package _622
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	alpabets := []int {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10}
+	str := ""
+	res := 0
+	fmt.Fscanln(r, &str)
+
+	for i := 0; i < len(str); i++  {
+		ch := str[i] - 'A'
+		res += alpabets[ch]
+	}
+
+	fmt.Fprintln(w, res)
+
+
+
+}
