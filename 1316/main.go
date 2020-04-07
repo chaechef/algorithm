b8ed68b8d129b212773d454e7f7235c2c73c1150
chaechef <chaeefe@gmail.com>
2020-04-07 17:16:28 +0900

    그룹단어체크

--- 1316/main.go
+++ 1316/main.go
@@ -1,1 +1,41 @@
-package _316
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func isGroupWord(str string) bool {
+	alphabets := [26]bool{}
+	check := true
+	var prev byte
+
+	for i := 0; i < len(str); i++ {
+		ch := str[i] - 'a'
+		if alphabets[ch] == true && prev != ch {
+			check = false
+			break
+		}
+		alphabets[ch] = true
+		prev = ch
+	}
+	return  check
+}
+func main()  {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var t, res int
+
+	fmt.Fscanln(r, &t)
+	for i := 0; i < t; i++ {
+		var str string
+		fmt.Fscanln(r, &str)
+		if isGroupWord(str) == true {
+			res++
+		}
+	}
+	fmt.Fprintln(w, res)
+}
