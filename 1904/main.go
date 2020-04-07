beb1acd351979421f60fbea3fb361fdb2ecac4ac
chaechef <chaeefe@gmail.com>
2020-04-07 17:19:52 +0900

    01타일

--- 1904/main.go
+++ 1904/main.go
@@ -1,1 +1,27 @@
-package _904
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var(
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+	arr [1000001]int
+	n int
+)
+
+func main() {
+	defer w.Flush()
+
+	fmt.Fscanf(r, "%d ", &n)
+	arr[1] = 1  // 1
+	arr[2] = 2 // 11 00
+	for i := 3; i <= n; i++ {
+		arr[i] = ((arr[i-2] % 15746) + (arr[i-1] % 15746) )% 15746
+	}
+	fmt.Fprintln(w,arr[n])
+
+}
