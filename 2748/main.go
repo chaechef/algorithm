808b32490dfa3ea9fda9b9dcb38ddba4cdb3a3bd
chaechef <chaeefe@gmail.com>
2020-04-07 17:22:16 +0900

    피보나치수2

--- 2748/main.go
+++ 2748/main.go
@@ -1,1 +1,26 @@
-package _748
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+	n int
+	arr [91]int64
+)
+
+func main() {
+	defer w.Flush()
+	fmt.Fscanf(r,"%d ", &n)
+	arr[0] = 0
+	arr[1] = 1
+	for i := 2; i < n + 1; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+
+	fmt.Fprintln(w,arr[n])
+}
