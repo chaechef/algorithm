d84fb632e9f3557e8cc8bd48582d29507fdbbe2b
chaechef <chaeefe@gmail.com>
2020-04-07 17:17:38 +0900

    n-queen

--- 9663/main.go
+++ 9663/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+var board [15]int
+var res, n int
+
+func main() {
+	defer w.Flush()
+	fmt.Fscanln(r, &n)
+	nqueen(0)
+	fmt.Fprintln(w, res)
+}
+
+func nqueen(curr int)  {
+	if curr == n {
+		res++
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		check := true
+		for j := 0; j < curr; j++ {
+			if board[j] == i || math.Abs(float64(curr-j)) == math.Abs(float64(board[j] - i)) {
+				check= false
+				break
+			}
+		}
+		if check {
+			board[curr] = i
+			nqueen(curr +1)
+		}
+	}
+
+}
--- 9663/sub.go
+++ 9663/sub.go
@@ -1,4 +1,4 @@
-package main
+package _9663
 
 import (
 	"bufio"
