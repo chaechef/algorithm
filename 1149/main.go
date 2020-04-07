fd33b506e54e710000b8de6b8d7257bdf26ee7b8
chaechef <chaeefe@gmail.com>
2020-04-07 17:14:17 +0900

    rgb거리

--- 1149/main.go
+++ 1149/main.go
@@ -1,1 +1,38 @@
-package _149
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var(
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+	num int
+	minr, ming, minb [1001]int
+	rcost, gcost, bcost int
+)
+
+func main() {
+	defer w.Flush()
+	fmt.Fscanf(r, "%d ", &num)
+	//init
+	fmt.Fscanf(r,"%d %d %d ", &rcost, &gcost, &bcost)
+	minr[1] = rcost
+	ming[1] = gcost
+	minb[1] = bcost
+	//loop
+	for i := 2; i < num+1; i++ {
+		fmt.Fscanf(r,"%d %d %d ", &rcost, &gcost, &bcost)
+		minr[i] = rcost + int(math.Min(float64(ming[i-1]),float64(minb[i-1])))
+		ming[i] = gcost + int(math.Min(float64(minr[i-1]),float64(minb[i-1])))
+		minb[i] = bcost + int(math.Min(float64(minr[i-1]),float64(ming[i-1])))
+
+	}
+	fmt.Fprintln(w,math.Min(float64(minr[num]),math.Min(float64(ming[num]),float64(minb[num]))))
+
+
+
+}
