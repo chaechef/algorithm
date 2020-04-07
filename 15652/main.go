b5ebf2e4e07af9c50cb35a7aca53c30917612a58
chaechef <chaeefe@gmail.com>
2020-04-07 17:19:37 +0900

    n과m 4

--- 15652/main.go
+++ 15652/main.go
@@ -1,1 +1,40 @@
-package _5652
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var r = bufio.NewReader(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func backtracking(picked []int, num int, topick int)  {
+	if len(picked) >= 2 && picked[len(picked)-2] > picked[len(picked)-1] {
+		return
+	}
+
+
+	if topick == 0 {
+		//print
+		for i := 0; i < len(picked); i++ {
+			fmt.Fprint(w, picked[i], " ")
+		}
+		fmt.Fprint(w, "\n")
+		return
+	}
+
+	for i := 1; i <= num; i++ {
+		picked = append(picked, i)
+		backtracking(picked, num, topick-1)
+		picked = picked[:len(picked)-1]
+	}
+}
+func main() {
+
+	defer w.Flush()
+	var n, m int
+	var arr []int
+	fmt.Fscanln(r, &n, &m)
+	backtracking( arr, n, m )
+}
