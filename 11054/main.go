1bd61978cb210b0b4302e212d4426f430e4e190c
chaechef <chaeefe@gmail.com>
2020-04-07 17:15:09 +0900

    가장 긴 바이토닉 부분 수열

--- 11054/main.go
+++ 11054/main.go
@@ -1,1 +1,56 @@
-package _1054
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+	n int
+	arr []int
+	dp1 []int
+	dp2 []int
+)
+
+func main() {
+	defer w.Flush()
+	fmt.Fscanf(r,"%d ", &n)
+	arr = make([]int, n)
+	dp1 = make([]int, n)
+	dp2 = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscanf(r,"%d ", &arr[i])
+	}
+
+	//to right
+	for i := 0; i < n; i++ {
+		dp1[i] = 1
+		for j := 0; j < i; j++ {
+			if arr[i] > arr[j] && dp1[j] + 1 > dp1[i]{
+				dp1[i] = dp1[j] + 1
+			}
+		}
+	}
+
+	//to reft
+	for i := n - 1; i >= 0; i-- {
+		dp2[i] = 1
+		for j := n-1; j > i ; j--  {
+			if arr[j] < arr[i] && dp2[i] < dp2[j] + 1 {
+				dp2[i] = dp2[j] + 1
+			}
+		}
+	}
+	res := 0
+	for i := 0; i < n; i++ {
+		if res < dp1[i] + dp2[i]{
+			res = dp1[i] + dp2[i]
+		}
+	}
+
+	fmt.Fprintln(w, res-1)
+
+}
