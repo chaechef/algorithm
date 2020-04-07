9073a28bd8be85d07f0e4f8b2c7057a34126fb6b
chaechef <chaeefe@gmail.com>
2020-04-07 17:22:38 +0900

    크로아티아알파벳

--- 2941/main.go
+++ 2941/main.go
@@ -1,1 +1,40 @@
-package _941
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func sub(str string) string {
+	tokens := []string {"c=","c-","d-","lj","nj","s=","z="}
+
+	if len(str) > 2 && str[:3] == "dz=" {
+		return str[3:]
+	}
+
+	for i := 0; i<len(tokens); i++  {
+		if len(str) > 1 && str[0:2] == tokens[i] {
+			return str[2:]
+		}
+	}
+
+	return str[1:]
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var str string
+	var res int
+	fmt.Fscanln(r, &str)
+
+	for str != "" {
+		str = sub(str)
+		res++
+	}
+	fmt.Fprintln(w, res)
+}
+
