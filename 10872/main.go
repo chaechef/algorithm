62e62f31b94f9768327da4c973148fa415d0cf27
chaechef <chaeefe@gmail.com>
2020-04-07 17:14:02 +0900

    팩토리얼

--- 10872/main.go
+++ 10872/main.go
@@ -1,1 +1,26 @@
-package _0872
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func factorial(num int) int {
+
+	if num == 0 {
+		return 1
+	}
+	return num * factorial(num-1)
+}
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	var num, res int
+	fmt.Fscanln(r, &num)
+	res = factorial(num)
+	fmt.Fprintln(w, res)
+
+}
