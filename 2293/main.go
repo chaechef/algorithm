89d96af6f9337d6a929874ecc262fe68d3b18b35
chaechef <chaeefe@gmail.com>
2020-04-12 14:53:51 +0900

    동전2

--- 2293/main.go
+++ 2293/main.go
@@ -28,20 +28,36 @@ func main() {
 	//output
 	for i := 1; i < k+1; i++ {
 		if i % coins[1] == 0 {
-			cache[i] = 1
+			cache[i] = i / coins[1]
+		}else {
+			cache[i] = -1
 		}
 	}
 	for i := 2; i < n+1; i++ {
 		for j := 1; j < k+1; j++ {
 			if j - coins[i] == 0 {
-				cache[j] = cache[j] + 1
+				cache[j] = 1
 			}else if j - coins[i] > 0 {
-				cache[j] = cache[j-coins[i]] + cache[j]
+				if cache[j-coins[i]] == -1 && cache[j] != -1 {
+					cache[j] = cache[j]
+				}else if cache[j-coins[i]] != -1 && cache[j] == -1 {
+					cache[j] = cache[j-coins[i]] + 1
+				}else if cache[j] != -1 && cache[j-coins[i]] != -1 {
+					if cache[j] > cache[j-coins[i]] + 1 {
+						cache[j] = cache[j-coins[i]] + 1
+					}else {
+						cache[j] = cache[j]
+					}
+				}else {
+					cache[j] = -1
+				}
 			}else{
 				cache[j] = cache[j]
 			}
 		}
+
 	}
+
 	fmt.Fprintln(w, cache[k])
 
 }
