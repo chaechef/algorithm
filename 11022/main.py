7ba72186bb1219fc1356ebccf76b026a09c09cae
chaechef <chaeefe@gmail.com>
2020-05-04 16:27:17 +0900

    A + B -8

---
+++ 11022/main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    testCase = int(input())
+    for i in range(testCase):
+        temp = list(map(int, input().split()))
+        print(f'Case #{i+1}: {temp[0]} + {temp[1]} = {temp[0]+temp[1]}')
