4cfe3ccc6b6815d956e5d8892e3b57d6f87ce6bf
mildchae <chaeefe@gmail.com>
2020-06-25 18:02:27 +0900

    다트게임

--- programmers/level1/다트게임.py
+++ programmers/level1/다트게임.py
@@ -1,35 +1,15 @@
 import re
 def solution(dartResult):
-    p = re.compile('\d+[SDT][*#]*')
+    p = re.compile('(\d+)([SDT])([*#]*)')
     m = p.findall(dartResult)
     ret = [0] * 3
-
+    bonus = {'S' : 1, 'D': 2, 'T': 3}
+    option = {'*' : 2, '#' : -1, "": 1}
     for idx, val in enumerate(m):
-        p1 = re.compile('\d+')
-        p2 = re.compile('[S|D|T]')
-        p3 = re.compile('[*|#]')
-        m1 = p1.findall(val)[0]
-        m2 = p2.findall(val)[0]
-        m3 = ""
-        if len(p3.findall(val)) > 0:
-            m3 = p3.findall(val)[0]
-        score = 0
-        if m2 == 'S':
-            score = int(m1) ** 1
-        elif m2 == 'D':
-            score = int(m1) ** 2
-        else:
-            score = int(m1) ** 3
-
-        ret[idx] = score
-
-        if m3 == '*':
-            if idx - 1 >= 0:
-                ret[idx-1] *= 2
-            ret[idx] *= 2
-        elif m3 == '#':
-            ret[idx] *= -1
-
+        print(idx,val)
+        if idx > 0 and val[2] == '*':
+            ret[idx-1] *= 2
+        ret[idx] = int(val[0]) ** bonus[val[1]] * option[val[2]]
 
     return sum(ret)
 
---
+++ programmers/level2/124나라의숫자.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = ''
+    print(int(str(n), 2))
+
+    return answer
+
+if __name__ == '__main__':
+    solution(103)
