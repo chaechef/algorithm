9dbe26bdd89e0ad924c2edb2da78b52f8df12bc3
mildchae <chaeefe@gmail.com>
2020-06-27 08:47:23 +0900

    124나라

--- programmers/level2/124나라의숫자.py
+++ programmers/level2/124나라의숫자.py
@@ -1,17 +1,22 @@
-def convert(n, base):
-    T = '012'
-    q, r = divmod(n, base)
-    print(q, r)
-    if q == 0:
-        return T[r]
-    else:
-        return convert(q, base) + T[r]
+# def convert(n, base):
+#     T = '012'
+#     q, r = divmod(n, base)
+#     print(q, r)
+#     if q == 0:
+#         return T[r]
+#     else:
+#         return convert(q, base) + T[r]
 
 def solution(n):
     answer = ''
-    cov = convert(n, 3)
-    print(cov)
+    covremain = {1: '1', 2: '2', 0: '4'}
+    while n:
+        n, r = divmod(n, 3)
+        answer = covremain[r] + answer
+        if r == 0:
+            n -= 1
+
     return answer
 
 if __name__ == '__main__':
-    solution(3)
+    print(solution(5))
