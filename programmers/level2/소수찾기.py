231a98c6c17e416f23f7bfe59aaf5b2e728a8ca3
mildchae <chaeefe@gmail.com>
2020-06-27 12:03:07 +0900

    소수찾기

---
+++ programmers/level2/소수찾기.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+def prime_set(num):
+    nums = set([i for i in range(3, num+1, 2)])
+
+    for i in [i for i in range(3, num+1, 2)]:
+        if i in nums:
+            nums -= set([j for j in range(i*2, num+1, i)])
+
+    nums.add(2)
+    return nums
+
+def solution(numbers):
+    answer = 0
+    numset = set([])
+
+    for i in range(1, len(numbers)+1):
+        temp = permutations(numbers, i)
+        for num in temp:
+            numset.add(int("".join(num)))
+
+
+    sprime = prime_set(max(numset))
+    for num in numset:
+        if num in sprime:
+            answer += 1
+
+    return answer
+
+
+print(solution("17"))
