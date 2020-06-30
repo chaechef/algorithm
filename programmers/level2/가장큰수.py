1d1ca3312f3d6584e5e59347a217a429efbf97b1
mildchae <chaeefe@gmail.com>
2020-07-01 08:48:49 +0900

    가장큰수

--- programmers/level2/가장큰수.py
+++ programmers/level2/가장큰수.py
@@ -1,58 +1,20 @@
-from collections import deque
-from itertools import permutations
+def cus(num):
+    s = str(num)
+    if len(s) == 3:
+        return int(s + s[0])
+    else:
+        return int(s * int((4 / len(s))))
+
+
 def solution(numbers):
-    answer = ''
-    a, b, c = [], [], []
-    for val in numbers:
-        if val < 10:
-            a.append(val)
-        elif val < 100:
-            b.append(val)
-        else:
-            c.append(val)
-    a = list(map(int, sorted(list(map(str, a)), reverse=True)))
-    b = list(map(int, sorted(list(map(str, b)), reverse=True)))
-    c = list(map(int, sorted(list(map(str, c)), reverse=True)))
 
-    a = deque(a)
-    b = deque(b)
-    c = deque(c)
-    while a or b or c:
+    numbers.sort(key=cus, reverse=True)
 
-        temp = []
-        tempidx = []
-        if a:
-            temp.append(a[0])
-            tempidx.append(0)
-        if b:
-            temp.append(b[0])
-            tempidx.append(1)
-        if c:
-            temp.append(c[0])
-            tempidx.append(2)
+    ret = "".join(map(str,numbers))
 
-        per = [int("".join(map(str, val))) for val in list(permutations(temp, len(temp)))]
+    # if int(ret) == 0:
+    #     return '0'
+    return ret
 
-        midx = per.index(max(per))
-        target = 0
-        if len(tempidx) == 3:
-            target = tempidx[midx // 2]
-        elif len(tempidx) == 2:
-            target = tempidx[midx // 1]
-        else:
-            target = tempidx[0]
+print(solution([1000,1000]))
 
-        if target == 0:
-            answer += str(a.popleft())
-        elif target == 1:
-            answer += str(b.popleft())
-        else:
-            answer += str(c.popleft())
-    if int(answer) == 0:
-        return '0'
-    return answer
-
-
-
-
-print(solution([999,9,998]))
