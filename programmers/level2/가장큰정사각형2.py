27b4003bfce1d9d59a290a2765d4e6f713df35b3
mildchae <chaeefe@gmail.com>
2020-07-02 13:43:57 +0900

    가장큰정사각형

--- programmers/level2/가장큰정사각형2.py
+++ programmers/level2/가장큰정사각형2.py
@@ -1,6 +1,24 @@
 def solution(board):
-   pass
+   allsum = sum([sum(line) for line in board])
+   if allsum == 0:
+      return 0
+
+   msize = 0
+   for i in range(1, len(board)):
+      for j in range(1, len(board[0])):
+         if board[i][j] == 0:
+            continue
+         board[i][j] = min(board[i][j-1], board[i-1][j], board[i-1][j-1]) + 1
+         if msize < board[i][j]:
+            msize = board[i][j]
+
+   if msize == 0:
+      return 1
+
+   return msize * msize
 
 
 
-solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]])
+
+
+print(solution([[0,0]]))
