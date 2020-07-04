081836bb6bb29d24125017011a4f728de555f5b4
mildchae <chaeefe@gmail.com>
2020-07-04 13:21:24 +0900

    조이스틱

--- programmers/level2/조이스틱.py
+++ programmers/level2/조이스틱.py
@@ -3,26 +3,28 @@ def caldist(char):
     return min(ord(char)- ord('A'), ord('Z')+1 - ord(char))
 
 def solution(name):
-    counts = [caldist(char) for char in name]
-    arr1 = [counts[0]] + counts[1:][::-1]
+    arr1 = [caldist(char) for char in name]
+    arr2 = [arr1[0]] + arr1[1:][::-1]
 
-    while arr1:
-        if arr1[-1] == 0:
-            arr1.pop()
-        else:
+    namesum = sum(arr1)
+    tempsum = 0
+    ret = -1
+    for val in arr1:
+        if tempsum == namesum:
             break
-    while counts:
-        if counts[-1] == 0:
-            counts.pop()
-        else:
+        tempsum += val
+        ret += val + 1
+
+    tempsum = 0
+    ret1 = -1
+    for val in arr2:
+        if tempsum == namesum:
             break
+        tempsum += val
+        ret1 += val + 1
 
-    ret = min(sum(arr1) + len(arr1)-1, sum(counts) + len(counts) - 1)
-    if ret == -1:
-        ret= 0
-    return ret
+    return min(ret, ret1)
 
-
-# print(solution("JEROEN"))
+print(solution("JEROEN"))
 # print(solution("JAN"))
-print(solution("A"))
+print(solution("AAA"))
