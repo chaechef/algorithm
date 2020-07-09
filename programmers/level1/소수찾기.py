9e7ac5afc20b4802ed49b21683e24fe6f5c6a6d3
mildchae <chaeefe@gmail.com>
2020-07-09 13:09:54 +0900

    소수찾기

--- programmers/level1/소수찾기.py
+++ programmers/level1/소수찾기.py
@@ -1,11 +1,10 @@
 def solution(n):
-    nums = set([i for i in range(3,n+1,2)])
+    nums = set(range(3, n+1, 2))
     for num in range(3, n+1, 2):
         if num in nums:
-            nums -= set([i for i in range(num*2, n+1, num)])
+            nums -= set(range(num*2, n+1, num))
 
     return len(nums) + 1
-
 
 
 if __name__ == "__main__":
