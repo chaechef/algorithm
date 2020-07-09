677fdcfc416beb9841e1ab96d348fab7c1d1669a
mildchae <chaeefe@gmail.com>
2020-07-09 12:22:54 +0900

    업데이트

--- programmers/level1/같은숫자는 싫어.py
+++ programmers/level1/같은숫자는 싫어.py
@@ -1,11 +1,6 @@
 def solution(arr):
-    arr2 = [arr[0]]
-    for i in range(1,len(arr)):
-        if arr2[-1] == arr[i]:
-            continue
-        else:
-            arr2.append(arr[i])
-    return arr2
+    answer = [val for idx, val in enumerate(arr) if val != arr[idx-1] or idx == 0]
+    return answer
 
 if __name__ == "__main__":
     solution([1,1,3,3,0,1,1])
