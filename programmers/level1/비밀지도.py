4f07ac845ff63ed0f94a87da97fbc94f45d6ad97
mildchae <chaeefe@gmail.com>
2020-07-09 13:00:05 +0900

    비밀지도

--- programmers/level1/비밀지도.py
+++ programmers/level1/비밀지도.py
@@ -1,9 +1,17 @@
-def t(string):
-    return "".join(['#' if c == '1' else ' ' for c in string])
+# def t(string):
+#     return "".join(['#' if c == '1' else ' ' for c in string])
+# def solution(n, arr1, arr2):
+#     answer = [a | b for a, b in zip(arr1, arr2)]
+#     binarr = list(map(lambda x: bin(x)[2:].zfill(n), answer))
+#     ret = [t(val) for val in binarr]
+#     return ret
+
+# --
+
 def solution(n, arr1, arr2):
-    answer = [a | b for a, b in zip(arr1, arr2)]
-    binarr = list(map(lambda x: bin(x)[2:].zfill(n), answer))
-    ret = [t(val) for val in binarr]
+    arr1, arr2 = [bin(val)[2:].rjust(n,'0') for val in arr1], [bin(val)[2:].rjust(n, '0') for val in arr2]
+    ret = [[str(int(aa)|int(bb)) for aa, bb in zip(a,b)] for a, b in zip(arr1, arr2)]
+    ret = ["".join(arr).replace("1", "#").replace('0', " ") for arr in ret]
     return ret
 
 if __name__ == '__main__':
