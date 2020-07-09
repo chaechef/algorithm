a43e61516e58b750732da8dacc98da5b73d313f8
mildchae <chaeefe@gmail.com>
2020-07-09 12:44:28 +0900

    다트게임

--- programmers/level1/다트게임.py
+++ programmers/level1/다트게임.py
@@ -1,17 +1,18 @@
 import re
 def solution(dartResult):
-    p = re.compile('(\d+)([SDT])([*#]*)')
-    m = p.findall(dartResult)
-    ret = [0] * 3
-    bonus = {'S' : 1, 'D': 2, 'T': 3}
-    option = {'*' : 2, '#' : -1, "": 1}
-    for idx, val in enumerate(m):
-        print(idx,val)
-        if idx > 0 and val[2] == '*':
-            ret[idx-1] *= 2
-        ret[idx] = int(val[0]) ** bonus[val[1]] * option[val[2]]
+    tok = re.findall('(\d+)([DST])([*#]?)', dartResult)
+    bonus = {'S':1, 'D':2, 'T':3}
+    option = {'#' : -1, '*': 2, '': 1}
+    print(tok)
+    scores = [0] * 3
+    for i in range(3):
+        scores[i] = int(tok[i][0]) ** bonus[tok[i][1]] * option[tok[i][2]]
+        if tok[i][2] == '*' and i > 0:
+            scores[i-1] *= 2
 
-    return sum(ret)
+    return sum(scores)
+
+
 
 if __name__ == '__main__':
     solution('1D2S#10S')
