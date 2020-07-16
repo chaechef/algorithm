641ac6ad2c8cee8b310c3fe95bda84276a11e354
mildchae <chaeefe@gmail.com>
2020-07-16 16:41:52 +0900

    등굣길

--- programmers/level3/등굣길.py
+++ programmers/level3/등굣길.py
@@ -1,52 +1,52 @@
 from collections import deque
-import sys
-sys.setrecursionlimit(10**9)
-def recursion(n,m, y,x):
-    if y == 0 and x == 0:
-        return 1
 
-    xy = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    ret = 0
-    for i in range(4):
-        cy, cx = y + xy[i][0], x + xy[i][1]
-        if 0 <= cy < n and 0 <= cx < m:
-            if cache[y][x] - 1 == cache[cy][cx]:
-                ret = (ret + recursion(n,m,cy,cx)) % 1000000007
+def bfs(n, m, board):
+    visit = [[False for j in range(m)] for i in range(n)]
+    count = [[100000000 for j in range(m)] for i in range(n)]
 
-    return ret
+    dq = deque([[0,0]])
+    count[0][0] = 1
+    xy = [[0,1],[1,0],[0,-1],[-1,0]]
+    step = 0
+    while dq:
+        size = len(dq)
+        for i in range(size):
+            cy, cx = dq.popleft()
+            if visit[cy][cx]:
+                continue
+            visit[cy][cx] = True
+            for d in xy:
+                ny, nx = cy + d[0], cx + d[1]
+                if ny < 0 or nx < 0 or ny >= n or nx >= m:
+                    continue
+                if board[ny][nx] == -1:
+                    continue
 
+                if not visit[ny][nx]:
+                    dq.append([ny,nx])
+                    if count[ny][nx] != 100000000:
+                        count[ny][nx] = (count[ny][nx] + count[cy][cx]) % 1000000007
+                    else:
+                        count[ny][nx] = count[cy][cx]
+
+        step += 1
+
+    if count[n-1][m-1] ==100000000:
+        return 0
+
+    return count[n-1][m-1]
 
 
 def solution(m, n, puddles):
-    global cache
-    global board
+    answer = 0
     board = [[0 for j in range(m)] for i in range(n)]
-    cache = [[-1 for j in range(m)] for i in range(n)]
 
     for puddle in puddles:
-        ta, tb = puddle
-        board[tb-1][ta-1] = -1
+        x, y = puddle
+        board[y-1][x-1] = -1
 
-    dq = deque([(0,0)])
-    step = 0
-    xy = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-    cache[0][0] = 0
-    while dq:
-        length = len(dq)
-        for _ in range(length):
-            curr = dq.popleft()
-            for i in range(4):
-                ny, nx = curr[0] + xy[i][0], curr[1] + xy[i][1]
-                if 0 <= ny < n and 0 <= nx < m and cache[ny][nx] == -1 and board[ny][nx] != -1:
-                    cache[ny][nx] = step + 1
-                    dq.append((ny, nx))
-        step += 1
-
-    if cache[n-1][m-1] == -1:
-        return 0
-    answer = recursion(n, m, n-1, m-1)
-
+    answer = bfs(n, m, board)
     return answer
 
 
-print(solution(4, 3, [[3,3],[2,3],[2,4]]))
+print(solution(4, 3, [[2,2]]))
