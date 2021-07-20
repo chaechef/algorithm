ec0474a34556b919811822e76d7e9660b0cddc43
hoonv <chaeefe@gmail.com>
2021-07-20 09:02:26 +0900

    remove unused code

--- algorithms/algorithms/String.swift
+++
@@ -1,9 +0,0 @@
-//
-//  String.swift
-//  algorithms
-//
-//  Created by 채훈기 on 2021/04/30.
-//
-
-import Foundation
-
--- etc/pick/main.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations
-
-if __name__ == "__main__":
-    arr = [123,345,332,123]
-    combi = combinations(arr, 2)
-    for val in combi:
-        print(val)
