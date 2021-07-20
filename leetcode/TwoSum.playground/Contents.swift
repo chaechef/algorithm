76a6b67f1a8abfb0a006515ff0a95687b40b9f2e
hoonv <chaeefe@gmail.com>
2021-07-20 09:16:02 +0900

    complete two sum

--- leetcode/TwoSum.playground/Contents.swift
+++ leetcode/TwoSum.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[i] + nums[j] == target {
+                    return [i, j]
+                }
+            }
+        }
+        return []
+    }
+}
+
+class Solution2 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var table: [Int:Int] = [:]
+        for i in 0..<nums.count {
+            if let index = table[target - nums[i]] {
+                return [index, i]
+            }
+            table[nums[i]] = i
+        }
+        return []
+    }
+}
+
+
+Solution().twoSum([1,2,3,4,5,6], 10)
