984842eb9ee44feb44d9a6a33baf40bf71db2840
hoonv <chaeefe@gmail.com>
2021-08-11 10:30:19 +0900

    add 2442

---
+++ boj/기초 코드 작성 요령 II/2442.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let num = Int(readLine()!)!
+
+for i in (1...num) {
+    let str = String(repeating: " ", count: num - i) + String(repeating: "*", count: 2 * i - 1)
+    print(str)
+}
